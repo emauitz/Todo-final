2c88a0c5c3544fcf2c1082c7b7181d54351fcec5
emauitz <emauitz@gmail.com>
2024-11-03 19:44:15 -0300

    cambios en dist

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
+  base: '/Todo-final/', // Asegúrate de que esto comience con una barra
+});
