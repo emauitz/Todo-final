e4435f79ea87b645b69d88bb01c4411cdf17a28a
emauitz <emauitz@gmail.com>
2024-11-06 19:57:49 -0300

    se configuran variables de entorno en vercel y se pushea para realizar deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Todo-final/', // Asegúrate de que esto comience con una barra
+  base: '',
 });
